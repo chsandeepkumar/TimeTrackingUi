5d7ada3d62b76cb579f7329b09705b93bafdb1ce
Uma Jangam <umajangam97@gmail.com>
2022-06-01 16:58:45 -0500

    Crreated ModelBinding(Passing values from View to Model Through Contoller)

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimeTracking_Ui.Models;
+
+namespace TimeTracking_Ui.Controllers
+{
+    public class RoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.message = "What is your Role";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Role role)
+        {
+            int Roleid = role.Roleid;
+            string? RoleName = role.RoleName;
+            string? RoleDescription = role.RoleDescription;
+            DateTime CreatedDateTime = role.CreatedDateTime;
+            DateTime UpdatedDateTime = role.UpdatedDateTime;
+            return View();
+        }
+
+
+    }
+
+
+}
---
+++ Models/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeTracking_Ui.Models
+{
+    public class Role
+    {
+        public int Roleid { get; set; }
+        public string RoleDescription { get; set; }
+        public string RoleName { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime UpdatedDateTime { get; set; }
+    }
+}
